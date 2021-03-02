9ecff19fb319789b7d26be56a6aead6e454c2d0e
Sayam Kumar <55996885+Sayam2801@users.noreply.github.com>
2021-03-03 02:07:07 +0530

    Third Commit

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ app.get("/", function (req, res) {
       console.log(response.data);
       return response.data;
     })
-    .then(({ btcinr, xrpinr }) => {
+    .then(({ btcinr, xrpinr, ethinr, trxinr, eosinr, zilinr, batinr, usdtinr, wrxinr, maticinr }) => {
       res.render("index", {
-        name1: "name1",
         last1: btcinr.last,
         buy1: btcinr.buy,
         sell1: btcinr.sell,
@@ -31,7 +30,47 @@ app.get("/", function (req, res) {
         buy2: xrpinr.buy,
         sell2: xrpinr.sell,
         volume2: xrpinr.volume,
-        base_unit2: "xrp"
+        base_unit2: "xrp",
+        last3: ethinr.last,
+        buy3: ethinr.buy,
+        sell3: ethinr.sell,
+        volume3: ethinr.volume,
+        base_unit3: "eth",
+        last4: trxinr.last,
+        buy4: trxinr.buy,
+        sell4: trxinr.sell,
+        volume4: trxinr.volume,
+        base_unit4: "trx",
+        last5: eosinr.last,
+        buy5: eosinr.buy,
+        sell5: eosinr.sell,
+        volume5: eosinr.volume,
+        base_unit5: "eos",
+        last6: zilinr.last,
+        buy6: zilinr.buy,
+        sell6: zilinr.sell,
+        volume6: zilinr.volume,
+        base_unit6: "zil",
+        last7: batinr.last,
+        buy7: batinr.buy,
+        sell7: batinr.sell,
+        volume7: batinr.volume,
+        base_unit7: "bat",
+        last8: usdtinr.last,
+        buy8: usdtinr.buy,
+        sell8: usdtinr.sell,
+        volume8: usdtinr.volume,
+        base_unit8: "usdt",
+        last9: wrxinr.last,
+        buy9: wrxinr.buy,
+        sell9: wrxinr.sell,
+        volume9: wrxinr.volume,
+        base_unit9: "wrx",
+        last10: maticinr.last,
+        buy10: maticinr.buy,
+        sell10: maticinr.sell,
+        volume10: maticinr.volume,
+        base_unit10: "matic"
       });
     });
 });
