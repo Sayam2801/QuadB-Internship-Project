370f143acee94a56aee74f09191422f2894d2c74
Sayam Kumar <55996885+Sayam2801@users.noreply.github.com>
2021-03-03 00:03:50 +0530

    Commit 3

--- app.js
+++ app.js
@@ -1,52 +1,41 @@
 //jshint esversion:6
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const https = require('https');
-const ejs = require('ejs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const ejs = require("ejs");
 const app = express();
-const axios = require('axios').default;
+const axios = require("axios").default;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
-app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get("/", function(req,res) {
-  const url1 = "https://api.wazirx.com/api/v2/tickers/btcinr";
-  if(response.statusCode === "200")
-  {
-    https.request(url, function(response) {
-      response.on("data", function(data) {
-        const apiData = JSON.parse(data);
-        res.render("list", {
-          name1: "BTC/INR",
-          last1: apiData.last,
-          buy1: apiData.buy,
-          sell1: apiData.sell,
-          volume1: apiData.vol,
-          base_unit1: "btc"
-        });
+app.get("/", function (req, res) {
+  axios
+    .get("https://api.wazirx.com/api/v2/tickers")
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .then(({ btcinr, xrpinr }) => {
+      res.render("index", {
+        name1: "name1",
+        last1: btcinr.last,
+        buy1: btcinr.buy,
+        sell1: btcinr.sell,
+        volume1: btcinr.volume,
+        base_unit1: "btc",
+        name2: "name2",
+        last2: xrpinr.last,
+        buy2: xrpinr.buy,
+        sell2: xrpinr.sell,
+        volume2: xrpinr.volume,
+        base_unit2: "xrp"
       });
     });
-  }
-  const url2 = "https://api.wazirx.com/api/v2/tickers/xrpinr";
-  if(response.statusCode === "200")
-  {
-    https.request(url, function(response) {
-      response.on("data", function(data) {
-        const apiData = JSON.parse(data);
-        res.render("list", {
-          name2: "BTC/INR",
-          last2: apiData.last,
-          buy2: apiData.buy,
-          sell2: apiData.sell,
-          volume2: apiData.vol,
-          base_unit2: "btc"
-        });
-      });
-    });
-  }
 });
-app.listen(3000, function() {
+
+app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
